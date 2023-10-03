58cd70309ac45a948201e8830dcf58b9ea94518a
dandr94 <d.andreev1994@gmail.com>
2023-10-04 02:25:21 +0300

    Added create_session and get_cookies functions

--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 import os
+import requests
+from dotenv import load_dotenv
 
-cookies = {
-    'cck1': os.getenv('CCK1'),
-    'lg_pref': os.getenv('LG_PREF'),
-    'webtools_session': os.getenv('WEBTOOLS_SEASON'),
-    'JSESSIONID': os.getenv('JSESSIONID')
-}
+load_dotenv()
+
+
+def create_session() -> requests.Session:
+    session = requests.Session()
+    return session
+
+
+def get_cookies() -> dict:
+    cookies = {
+        'cck1': os.getenv('CCK1'),
+        'JSESSIONID': os.getenv('JSESSIONID'),
+        'ln_pref': os.getenv('LG_PREF')
+    }
+
+    return cookies
 
 
 def fetch_response(session, url, cookies, params=None):
