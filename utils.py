15359e442664dab3c6569e7f48e77a1b4b2b6aec
dandr94 <d.andreev1994@gmail.com>
2023-10-05 07:48:45 +0300

    add get_current_time and time_left_until_all_data_is_fetched funcs for better feedback

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import requests
 from dotenv import load_dotenv
 
@@ -22,7 +24,20 @@ def get_cookies() -> dict:
 
 def fetch_response(session, url, cookies, params=None):
     response = session.get(url, cookies=cookies, allow_redirects=False, params=params)
-
     if response.status_code == 200:
         return response
     return None
+
+
+def get_current_time():
+    return time.strftime("%H:%M:%S", time.localtime())
+
+
+def time_left_until_all_data_is_fetched(documents_left):
+    seconds = documents_left
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    return f'{hours}h {minutes}m {seconds}s'
