a140a2016680cd236835060197e924c629bcde35
dandr94 <d.andreev1994@gmail.com>
2023-10-18 20:49:23 +0300

    Continue to improve readibility in the main func, needs more work

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import time
 
 from bs4 import BeautifulSoup
-import logging
 from data_handling import load_existing_data, OUTPUT_FILE, load_state, save_state, save_data
 from data_scrapper import scrape_ted_data, fetch_hrefs, parse_url, get_last_page, SEARCH_URL, BASE_WEBSITE
-from utils import fetch_response, create_session, get_cookies, get_current_time, time_left_until_all_data_is_fetched, \
-    TextFormatter
+from utils import fetch_response, create_session, get_cookies, get_current_time, TextFormatter, url_is_scrapped, Logger, \
+    update_has_reach_last_scrapped_url, MessageProvider, action_is_update
 from user_interface import get_user_choice_for_action, default_app_message
 
 REQUEST_DELAY = 1
-
-
-def configure_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        filename='app.log',
-        filemode='a'
-    )
+MAXIMUM_DOCUMENTS_PER_PAGE = 25
 
 
 def main() -> None:
-    configure_logging()
+    logger = Logger()
+    message_provider = MessageProvider()
     text_formatter = TextFormatter()
 
     session = create_session()
@@ -41,15 +33,15 @@ def main() -> None:
     if state:
         action = get_user_choice_for_action()
 
-        if action == '2':
+        if action_is_update(action):
             last_processed_page = 1
 
     try:
         response = fetch_response(session, SEARCH_URL, cookies)
 
         if not response:
-            print(text_formatter.format_message_fail(f'Failed to retrieve the URL "{SEARCH_URL}".'))
-            logging.error(f'Failed to retrieve the URL "{SEARCH_URL}".')
+            print(text_formatter.format_message_fail(message_provider.message_failed_to_retrieve_url(SEARCH_URL)))
+            logger.log_error(message_provider.message_failed_to_retrieve_url(SEARCH_URL))
 
             return
 
@@ -57,8 +49,10 @@ def main() -> None:
         last_page_number = get_last_page(soup.find('div', class_='page-icon pagelast'))
 
         if not last_page_number:
-            print(text_formatter.format_message_fail('Failed to retrieve last page...stopping the process'))
-            logging.error('Failed to retrieve last page...stopping the process')
+            print(text_formatter.format_message_fail(message_provider.message_failed_to_retrieve_last_page()))
+
+            logger.log_error(message_provider.message_failed_to_retrieve_last_page())
+
             return
 
         for page in range(last_processed_page, last_page_number):
@@ -67,11 +61,14 @@ def main() -> None:
 
             if not hrefs:
                 print(text_formatter.format_message_fail(
-                    f'Failed to retrieve page {page}! Status code: {response.status_code}'))
-                logging.warning(f'Failed to retrieve page {page}! Status code: {response.status_code}')
+                    message_provider.message_failed_to_retrieve_page(page, response.status_code)))
+
+                logger.log_warning(message_provider.message_failed_to_retrieve_page(page, response.status_code))
+
                 continue
 
             state['last_processed_page'] = page
+
             save_state(state)
 
             for href in hrefs:
@@ -79,63 +76,63 @@ def main() -> None:
                 data_url = BASE_WEBSITE + current_url
                 document_main_url = BASE_WEBSITE + href
 
-                if document_main_url in existing_links and action == '1':
+                if url_is_scrapped(document_main_url, existing_links, action):
                     print(text_formatter.format_message_work_in_progress(
-                        f'Continuing to next URL on page {page}, because this one is already scrapped: {document_main_url}'))
+                        message_provider.message_url_is_scrapped(page, document_main_url)))
 
-                    logging.info(
-                        f'Continuing to the next URL on page {page}, because this one is already scrapped: {document_main_url}')
+                    logger.log_info(message_provider.message_url_is_scrapped(page, document_main_url))
 
                     continue
 
-                if document_main_url in existing_links and action == '2':
-                    print(text_formatter.format_message_success('Data successfully updated.'))
-                    logging.info('Data successfully updated.')
+                if update_has_reach_last_scrapped_url(document_main_url, existing_links, action):
+                    print(text_formatter.format_message_success(
+                        message_provider.message_update_has_reach_last_scrapped_url()))
+
+                    logger.log_info(message_provider.message_update_has_reach_last_scrapped_url())
+
                     return
 
-                print(text_formatter.format_message_work_in_progress(
-                    f'[{get_current_time()}] - Working on URL on page {page}/{last_page_number}: {current_url}...'))
+                print(text_formatter.format_message_work_in_progress(message_provider.construct_message_with_time_stamp(
+                    message_provider.message_work_in_progress(page, last_page_number, current_url))))
 
                 data = scrape_ted_data(session, cookies, data_url, document_main_url)
 
                 if data:
                     all_data.append(data)
 
-                    print(text_formatter.format_message_success(
-                        f'[{get_current_time()}] - Successfully fetched data on page {page} from URL {data_url}'))
+                    print(text_formatter.format_message_success(message_provider.construct_message_with_time_stamp(
+                        message_provider.message_successfully_scrapped_data(page, data_url))))
 
-                    logging.info(f'Successfully fetched data on page {page} from URL {data_url}')
+                    logger.log_info(message_provider.message_successfully_scrapped_data(page, data_url))
 
                     save_data(all_data, OUTPUT_FILE)
 
-                else:
-                    print(text_formatter.format_message_fail(
-                        f'[{get_current_time()}] - Impossible to fetch data from URL: {document_main_url} on page {page} because it does not '
-                        f'have a data page'))
+                    print(text_formatter.format_message_success(message_provider.construct_message_with_time_stamp(
+                        message_provider.message_successful_data_save(OUTPUT_FILE))))
 
-                    logging.warning(
-                        f'Impossible to fetch data from URL: {document_main_url} on page {page} because it does not '
-                        f'have a data page')
+                    logger.log_info(message_provider.message_successful_data_save(OUTPUT_FILE))
+                else:
+                    print(text_formatter.format_message_fail(message_provider.construct_message_with_time_stamp(
+                        message_provider.message_no_data_page(page, document_main_url))))
+
+                    logger.log_warning(message_provider.message_no_data_page(page, document_main_url))
 
                 save_state(state)
 
                 time.sleep(REQUEST_DELAY)
 
-            logging.info(f'[{get_current_time()}] - Data saved successfully to {OUTPUT_FILE}!')
-
-            if not action or action == '1':  # ETA but bad way of doing it
+            if not action_is_update(action):  # ETA but bad way of doing it
                 pages_remaining = last_page_number - state['last_processed_page']
-                documents_left = pages_remaining * (25 * REQUEST_DELAY)
-                print(text_formatter.format_message_work_in_progress(
-                    f'[{get_current_time()}] - Time left until all data is fetched: ~{time_left_until_all_data_is_fetched(documents_left)}'))
+                documents_left = pages_remaining * (MAXIMUM_DOCUMENTS_PER_PAGE * REQUEST_DELAY)
+                print(text_formatter.format_message_work_in_progress(message_provider.message_eta(documents_left)))
 
     except KeyboardInterrupt:
-        print("\nProcess interrupted by user. Saving data collected so far.")
-        logging.info("Process interrupted by user. Saving data collected so far.")
+        print(message_provider.message_interrupted_by_user())
+        logger.log_info(message_provider.message_interrupted_by_user())
 
     except Exception as e:
-        print(text_formatter.format_message_fail(f"An unexpected error occurred - {str(e)}"))
-        logging.error(f"An unexpected error occurred - {str(e)}")
+        print(text_formatter.format_message_fail(message_provider.message_unexpected_error_occurred(e)))
+        logger.log_error(message_provider.message_unexpected_error_occurred(e))
         save_data(all_data, OUTPUT_FILE)
 
 
