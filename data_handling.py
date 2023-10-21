8a677f96d3483380fbb3e270d4c9208a420289f8
dandr94 <d.andreev1994@gmail.com>
2023-10-21 17:44:27 +0300

    Remove redundant documentation

--- data_handling.py
+++ data_handling.py
@@ -9,14 +9,7 @@ STATE_FILE = 'state.json'
 def load_existing_data(filename: str) -> List[Dict[str, str]]:
     """
         Load existing data from a JSON file.
-
-        Args:
-            filename (str): The name of the JSON file to read data from.
-
-        Returns:
-            List[Dict[str, str]]: A list of dictionaries containing the loaded data.
-                An empty list is returned if the file is not found or an error occurs.
-        """
+    """
     try:
         with open(filename, 'r', encoding='utf-8') as json_file:
             existing_data = json.load(json_file)
@@ -28,10 +21,6 @@ def load_existing_data(filename: str) -> List[Dict[str, str]]:
 def save_data(data: List[Dict[str, str]], filename: str) -> None:
     """
     Save scraped data to a JSON file.
-
-    Args:
-        data (List[Dict[str, str]]): List of dictionaries containing scraped data.
-        filename (str): The name of the JSON file to save data to.
     """
     with open(filename, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
@@ -40,10 +29,6 @@ def save_data(data: List[Dict[str, str]], filename: str) -> None:
 def load_state() -> Dict:
     """
         Load the application state from a JSON file.
-
-        Returns:
-            Dict: A dictionary containing the loaded application state.
-                An empty dictionary is returned if the file is not found.
     """
     try:
         with open(STATE_FILE, 'r', encoding='utf-8') as state_file:
@@ -56,9 +41,6 @@ def load_state() -> Dict:
 def save_state(state: Dict) -> None:
     """
         Save the application state to a JSON file.
-
-        Args:
-            state (Dict): A dictionary containing the application state to be saved.
     """
     with open(STATE_FILE, 'w', encoding='utf-8') as state_file:
         json.dump(state, state_file, ensure_ascii=False, indent=4)
